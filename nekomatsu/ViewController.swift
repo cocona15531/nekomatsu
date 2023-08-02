2ae13a7d3bc332b84cb98ad928dcdd22bedd473e
issei.ueda <ron.ron633662@gmail.com>
2023-08-02 15:06:04 +0900

    Initial Commit

---
+++ nekomatsu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  nekomatsu
+//
+//  Created by Issei Ueda on 2023/08/02.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
